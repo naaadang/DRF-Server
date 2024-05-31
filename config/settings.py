e49b6852f0a90f17d8776d7c811a897a80bb98d4
naaadang <naaadang@naver.com>
2024-05-31 21:03:49 +0900

    백엔드 주소 추가

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,8 @@ DEBUG = 'True'
 ALLOWED_HOSTS=[
     'http://127.0.0.1:8000',
     '127.0.0.1',
+    #배포된 백엔드 주소
+    'port-0-drf-server-1mrfs72llwumekr6.sel5.cloudtype.app',
 ]
 
 #CORS 설정
@@ -40,11 +42,15 @@ CORS_ALLOW_CREDENTIALS=True
 CORS_ALLOWED_ORIGINS=[
     "http://localhost:3000",
     "http://127.0.0.1:8000",
+    #배포된 백엔드 주소
+    'https://port-0-drf-server-1mrfs72llwumekr6.sel5.cloudtype.app'
 ]
 #CSRF 설정
 CSRF_TRUSTED_ORIGINS=[
     "http://localhost:3000",
     "http://127.0.0.1:8000",
+    #배포된 백엔드 주소
+    'https://port-0-drf-server-1mrfs72llwumekr6.sel5.cloudtype.app'
 ]
 # Application definition
 
